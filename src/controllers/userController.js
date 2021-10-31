324a9953b109653dbedcc90737f1c9f87afb6d78
Marcos676 <patino-marcos@outlook.com>
2021-10-30 22:34:21 -0300

    add profile views and edit this

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,13 +4,13 @@ const path = require('path');
 
 
 module.exports = {
-    loginRegister: (req,res) => {
+    loginRegister: (req, res) => {
         res.render('./users/loginRegister')
     },
-    registerProcess:(req,res) => {
+    registerProcess: (req, res) => {
         let users = require('../data/users.json')
 
-        let {fullName, email, password, rePassword} = req.body
+        let { fullName, email, password, rePassword } = req.body
 
         if (password != rePassword) {
             res.render('./users/loginRegister')
@@ -36,9 +36,9 @@ module.exports = {
 
         return res.redirect('/usuario/perfil')
     },
-    loginProcess:(req,res) => {
+    loginProcess: (req, res) => {
         let users = require('../data/users.json')
-        let {email, password, rememberme} = req.body
+        let { email, password, rememberme } = req.body
 
         let user = users.find(user => user.email === email.trim())
 
@@ -46,17 +46,17 @@ module.exports = {
             return res.render('./users/loginRegister')
         }
 
-        if (!bcrypt.compareSync(password, user.password)){
+        if (!bcrypt.compareSync(password, user.password)) {
             return res.render('./users/loginRegister')
         }
 
         req.session.user = {
-            id : user.id,
+            id: user.id,
             email: user.email,
-            firstName : user.firstName,
-            lastName : user.lastName,
-            image : user.image,
-            admin : user.admin
+            firstName: user.firstName,
+            lastName: user.lastName,
+            image: user.image,
+            admin: user.admin
         }
 
         res.locals.localsUser = req.session.user
@@ -64,18 +64,62 @@ module.exports = {
         if (rememberme) {
             res.cookie('FootGoose', req.session.user, { maxAge: 1000 * 60 * 60 * 24 });
         }
-        
-        return res.redirect('/')
+
+        return res.redirect('/usuario/perfil')
     },
-    profile: (req,res) => {
+    profile: (req, res) => {
         if (!req.session.user) {
             return res.render('./users/loginRegister')
         }
         return res.render('./users/profile')
     },
-    logout: (req,res) => {
+    editProfile: (req, res) => {
+        let users = require('../data/users.json')
+        let user = users.find(user => user.id == req.session.user.id)
+        return res.render('./users/profileEdit', {
+            user
+        })
+    },
+    editProfileProcess: (req, res) => {
+        let users = require('../data/users.json')
+        let { fullName, email, address, tel, pass1 } = req.body
+
+        let user = users.find(user => user.id == req.session.user.id)
+
+        let [firstName, lastName] = fullName.split(' ')
+
+        let userEdited = {
+            id: user.id,
+            firstName,
+            lastName,
+            email,
+            password: pass1 ? bcrypt.hashSync(pass1, 10) : user.password,
+            address,
+            image: req.file ? req.file.filename : user.image,
+            tel,
+            admin: user.admin
+        }
+
+        let usersModified = users.map(user => user.id == userEdited.id ? userEdited : user)
+
+        fs.writeFileSync(path.join(__dirname, '../data/users.json'), JSON.stringify(usersModified, null, 2), 'utf-8')
+
+        req.session.user = {
+            id: userEdited.id,
+            email: userEdited.email,
+            firstName: userEdited.firstName,
+            lastName: userEdited.lastName,
+            image: userEdited.image,
+            admin: userEdited.admin
+        }
+
+        res.locals.localsUser = req.session.user
+
+        return res.render('./users/profile')
+    },
+    logout: (req, res) => {
         delete req.session.user;
-        
+
         if (req.cookies.FootGoose) {
             res.cookie('FootGoose', '', { maxAge: -1 });
         }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,18 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-const {loginRegister, profile, loginProcess, registerProcess, logout} = require('../controllers/userController')
+const {loginRegister, profile, loginProcess, registerProcess, logout, editProfile, editProfileProcess} = require('../controllers/userController')
 
 //middlewares
+let uploadAvatar = require('../middlewares/uploadAvatar')
 let userCheck = require('../middlewares/userCheck');
 let userloginCheck = require('../middlewares/userloginCheck');
-
 /* GET home page. */
 
 router.get('/ingresar', userCheck, loginRegister);
 router.post('/registro', registerProcess)
 router.post('/login', loginProcess)
 router.get('/perfil', userloginCheck, profile);
-router.get('/logout', logout);
+router.get('/logout', userloginCheck, logout);
+router.get('/editar', userloginCheck, editProfile);
+router.put('/editar', uploadAvatar.single('img'), userloginCheck, editProfileProcess);
 
 module.exports = router;
